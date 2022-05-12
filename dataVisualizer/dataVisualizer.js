47defae89c3d5094f19b00b272cf4649081e3189
Edracoon <77042040+Edracoon@users.noreply.github.com>
2022-05-12 19:04:50 +0200

    not working for the moment

--- dataVisualizer/dataVisualizer.js
+++ dataVisualizer/dataVisualizer.js
@@ -1,4 +1,11 @@
-let ctx = document.getElementById("graph").getContext('2d');
+let context1 = document.getElementById("graph1").getContext('2d');
+let context2 = document.getElementById("graph2").getContext('2d');
+let SVCfile = undefined;
+
+const learningRate = 0.1;
+let m = 0.0; // data length
+
+let learningTime = 0;
 
 let data = [];
 let dataPrint = [];
@@ -6,16 +13,12 @@ let dataPrint = [];
 let theta0 = 0;
 let theta1 = 0;
 
-let m = undefined;
-const ratioLearning = 0.1;
-
-function prixEstime(kilométrage) {
-    return theta0 + (theta1 * kilométrage);
-}
-
 
 
 async function getSVC(file) {
+    console.log(file);
+    if (data.length || SVCfile !== undefined || file.type !== "text/csv")
+        return ;
     let text = await file.text();
     text = text.split('\n');
     text.forEach((value) => {
@@ -30,46 +33,91 @@ async function getSVC(file) {
     });
 
     dataPrint = data.map((value) => ({x: value.km, y: value.price}));
-
-    m = data.length;
-
-    console.log(dataPrint);
     
     launchGraph();
-
 }
 
-function getSumTheta0() {
+function estimatePrice(kilometters, theta0, theta1) {
+    return theta0 + (theta1 * kilometters);
+}
+
+function getSumTheta0(kilometters, prices, tmpTheta0, tmpTheta1) {
     sum = 0;
-    for (let i = 0 ; i < m ; i++)
-        sum += prixEstime(kilométrage[i]) - prix[i];
+    for (let i = 0 ; i < data.length ; i++)
+        sum += estimatePrice(kilometters[i], tmpTheta0, tmpTheta1) - prices[i];
     return sum;
 }
 
-function getSumTheta1() {
+function getSumTheta1(kilometters, prices, tmpTheta0, tmpTheta1) {
     sum = 0;
-    for (let i = 0 ; i < m ; i++)
-        sum += (prixEstime(kilométrage[i]) - prix[i]) * kilométrage[i];
+    for (let i = 0 ; i < data.length ; i++)
+        sum += (estimatePrice(kilometters[i], tmpTheta0, tmpTheta1) - prices[i]) * kilometters[i];
     return sum;
 }
 
 function linearRegression() {
+    if (!data.length) {
+        console.log("linearRegression: Need data !");
+        return ;
+    }
 
-    some = get
-    tmpTheta0 = ratioLearning * 1 / m * some;
+    m = data.length;
+    console.log("m", m);
+    let kilometters = data.map((value) => parseInt(value.km));
+    let prices = data.map((value) => parseInt(value.price));
 
+    console.log(`====== learningTime[${++learningTime}] ======\n`);
+
+    // Initialize tmp values
+    let tmpTheta0 = theta0;
+    let tmpTheta1 = theta1;
+    console.log('prev theta0 -> ', theta0, '\nprev theta1 -> ', theta1);
+
+    // Compute theta with tmp values
+    let sum0 = getSumTheta0(kilometters, prices, theta0, theta1);
+    let sum1 = getSumTheta1(kilometters, prices, theta0, theta1);
+    tmpTheta0 = learningRate * (1.0 / m) * sum0;
+    tmpTheta1 = learningRate * (1.0 / m) * sum1;
+
+    // Update global theta
+    theta0 = tmpTheta0;
+    theta1 = tmpTheta1;
+
+    console.log('computed theta0 -> ', tmpTheta0, '\ncomputed theta1 ->', tmpTheta1);
+
+    // console.log(theta0, theta1)
+
+    // return new Chart(context2, {
+    //     type: 'scatter',
+    //     data: {
+    //         datasets: [{
+    //             label: 'X: Kilometers, Y: Price',
+    //             showLine: true,
+    //             data: dataPrint,
+    //             backgroundColor: 'rgb(255, 99, 132)'
+    //         }]
+    //     },
+    //     options: {
+    //         scales: {
+    //             x: {
+    //                 type: 'linear',
+    //                 position: 'bottom'
+    //             }
+    //         }
+    //     }
+    // });
 }
 
 function launchGraph() {
 
-    return new Chart(ctx, {
+    return new Chart(context1, {
         type: 'scatter',
         data: {
             datasets: [{
                 label: 'X: Kilometers, Y: Price',
                 showLine: true,
                 data: dataPrint,
-                backgroundColor: 'rgb(255, 99, 132)'
+                backgroundColor: 'rgb(255, 0, 0)'
             }]
         },
         options: {
@@ -81,5 +129,4 @@ function launchGraph() {
             }
         }
     });
-
 }
